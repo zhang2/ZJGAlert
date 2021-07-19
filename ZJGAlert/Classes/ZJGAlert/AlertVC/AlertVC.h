5f2fe732ac59363e4bdd6243275e9194e61cfbcf
zjg <zhang1314520648@163.com>
2021-07-19 15:58:45 +0800

    更新 tag 2.1.0

---
+++ ZJGAlert/Classes/ZJGAlert/AlertVC/AlertVC.h
@@ -0,0 +1,16 @@
+//
+//  AlertVC.h
+//  ZJGAlert
+//
+//  Created by zjg on 7/19/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AlertVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
